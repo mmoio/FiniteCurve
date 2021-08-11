ba2610af1a9051f9c18adf2b268204f750b2c144
Michele Moio <michele.moio.4d@gmail.com>
2021-08-11 22:09:15 +0200

    removed demo component

--- src/CustomButton.tsx
+++
@@ -1,6 +0,0 @@
-import { Button, Alert } from 'react-native';
-import React from 'react';
-
-export const CustomButton = () => {
-	return <Button title='custom' onPress={() => Alert.alert('pressme')} />;
-};
