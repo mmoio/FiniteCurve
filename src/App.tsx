e19a63888d3764e7c703f8c9924889baca5d8227
Michele Moio <michele.moio.4d@gmail.com>
2021-08-12 12:35:31 +0200

    added react navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import {
-	ApolloClient,
-	ApolloProvider,
-	InMemoryCache,
-	NormalizedCacheObject,
-} from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache, NormalizedCacheObject } from '@apollo/client';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
+import Config from 'react-native-config';
 import FiniteCurveScreen from './views/FiniteCurve';
 import StorybookScreen from './views/StorybookScreen';
-import Config from 'react-native-config';
 
 // Initialize Apollo Client
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
@@ -16,12 +13,16 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
 });
 
 const IS_STORYBOOK = Config.LOAD_STORYBOOK === 'true';
+const Stack = createNativeStackNavigator();
 
 const App: React.FC = () => (
 	<ApolloProvider client={client}>
-		{ IS_STORYBOOK? <StorybookScreen/> : <FiniteCurveScreen/> }
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name='Home' component={IS_STORYBOOK ? StorybookScreen : FiniteCurveScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
 	</ApolloProvider>
 );
 
 export default App;
-
