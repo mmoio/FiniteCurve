19c00608dfbd76c02954c636c33c15ad72cd3c5a
Michele Moio <michele.moio.4d@gmail.com>
2021-08-12 11:39:20 +0200

    storybook loader fix

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -3,9 +3,15 @@
 //
 // https://github.com/elderfo/react-native-storybook-loader.git
 
-function loadStories() {}
+function loadStories() {
+	require('../src/characters/components/Character/stories');
+	require('../src/characters/components/CharacterList/stories');
+}
 
-const stories = [];
+const stories = [
+	'../src/characters/components/Character/stories',
+	'../src/characters/components/CharacterList/stories',
+];
 
 module.exports = {
 	loadStories,
